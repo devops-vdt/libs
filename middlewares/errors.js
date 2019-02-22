44fbc2b124afe1bcc332ce5a5349e9eb7aef34d6
Ricardo <DevVDT@Devs-MacBook-Pro.local>
2019-02-22 14:14:55 -0300

    Fix errors handlers middlewares

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -1,12 +1,22 @@
 
 function errors(err, req, res, next){
-    const error = process.env.APP_DEBUG ? require('serialize-error')(err) : { message: 'An error occurred' };
-    const status = err.statusCode || 500;
+    const serializeError = require('serialize-error');
+    let status = 500;
+    const error = process.env.APP_DEBUG ? serializeError(err) : { message: 'An error occurred' };
+    
+    // if we are setting the status code before, we problably are treating the message too
+    if (err.statusCode) {
+        status = err.statusCode;
+
+        if (!process.env.APP_DEBUG) {
+            error.message = err.message || error.message
+        }
+    }
 
     //respond to client
     res.status(status).json({ error });
 
-    logger.log("errors", error);
+    logger.log("errors", serializeError(err));
 }
 
 module.exports = errors;
