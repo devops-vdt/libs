bbaf06fcc913d0155795d2e65105aefb1891cb66
Ricardo <DevVDT@Devs-MacBook-Pro.local>
2019-02-22 14:28:25 -0300

    Add utils functions

--- logger/index.js
+++ logger/index.js
@@ -1,18 +1,4 @@
-function getDate(pattern) {
-    const year = (new Date().getUTCFullYear()).toString().padStart(4, '0');
-    const month = (new Date().getUTCMonth() + 1).toString().padStart(2, '0');
-    const day = (new Date().getUTCDate()).toString().padStart(2, '0');
-
-    const hour = (new Date().getHours()).toString().padStart(2, '0');
-    const minute = (new Date().getMinutes()).toString().padStart(2, '0');
-
-    return pattern
-        .replace('Y', year)
-        .replace('m', month)
-        .replace('d', day)
-        .replace('H', hour)
-        .replace('i', minute);
-}
+const getDate = require('../utils/date');
 
 module.exports = (config) => {
     if (!config) {
