fd44fe17e8c1af3ef171faae11e55f3862578d25
Luccas Almeida <luccas.almeida@vadetaxi.com.br>
2019-07-10 16:08:09 -0300

    fix elasticsearch

--- database/drivers/Elasticsearch.js
+++ database/drivers/Elasticsearch.js
@@ -1,150 +1,153 @@
-const Driver = require('./Driver');
+const Driver = require("./Driver");
 
 class Elasticsearch extends Driver {
-    constructor(client) {
-        super();
+  constructor(client) {
+    super();
 
-        this.client = client;
-    }
+    this.client = client;
+  }
 
-    initialize(model) {
-        model.client = this.client;
-        model.index = model.table;
-        model.pk = model.pk || 'id';
+  initialize(model) {
+    model.client = this.client;
+    model.index = model.table;
+    model.pk = model.pk || "id";
 
-        model.find = this.find(model);
-        model.insert = this.insert(model);
-        model.updateById = this.updateById(model, model.insert);
-        model.deleteById = this.deleteById(model);
-    }
+    model.find = this.find(model);
+    model.insert = this.insert(model);
+    model.updateById = this.updateById(model, model.insert);
+    model.deleteById = this.deleteById(model);
+  }
 
-    insert(model) {
-        return (payload) => {
-            return new Promise((resolve, reject) => {
-                if (!payload[model.pk]) {
-                    throw new Error('Need to specify the pk on insertOrUpdate');
-                }
-
-                const document = {
-                    index: model.table,
-                    id: payload[model.pk],
-                    body: payload
-                };
-
-                this.client.index(document, (err, response, status) => {
-                    if (err) {
-                        reject(err);
-                    }
-
-                    resolve(response);
-                });
-            });
+  insert(model) {
+    return payload => {
+      return new Promise((resolve, reject) => {
+        if (!payload[model.pk]) {
+          throw new Error("Need to specify the pk on insertOrUpdate");
         }
-    }
 
-    updateById(model, insertOrUpdate) {
-        return (id, payload) => {
-            const newPayload = {
-                ...payload
-            };
-            newPayload[model.pk] = id;
+        const document = {
+          index: model.table,
+          type: "_doc",
+          id: payload[model.pk],
+          body: payload
+        };
 
-            return insertOrUpdate(newPayload);
-        }
-    }
+        this.client.index(document, (err, response, status) => {
+          if (err) {
+            reject(err);
+          }
 
-    deleteById(model) {
-        return (id) => {
-            return new Promise((resolve, reject) => {
-                const filter = {
-                    index: model.table,
-                    id
-                };
+          resolve(response);
+        });
+      });
+    };
+  }
 
-                this.client.delete(filter, (err, response, status) => {
-                    if (err) {
-                        reject(err);
-                    }
+  updateById(model, insertOrUpdate) {
+    return (id, payload) => {
+      const newPayload = {
+        ...payload
+      };
+      newPayload[model.pk] = id;
 
-                    resolve(response);
-                });
-            });
-        }
-    }
+      return insertOrUpdate(newPayload);
+    };
+  }
 
-    count(model) {
-        return () => {
-            return new Promise((resolve, reject) => {
-                const filter = {
-                    index: model.table
-                };
-                this.client.count(filter, (err, response, status) => {
-                    if (err) {
-                        reject(err);
-                    }
+  deleteById(model) {
+    return id => {
+      return new Promise((resolve, reject) => {
+        const filter = {
+          index: model.table,
+          type: "_doc",
+          id
+        };
 
-                    resolve(response);
-                });
-            });
-        }
-    }
+        this.client.delete(filter, (err, response, status) => {
+          if (err) {
+            reject(err);
+          }
 
-    find(model) {
-        return (query) => {
-            return new Promise((resolve, reject) => {
-                const treatFilters = (filters, q, prefix = '') => {
-                    for (let key of Object.keys(q)) {
-                        if (prefix) {
-                            prefix += '.'
-                        }
-                        const keyField = `${prefix}${key}`;
+          resolve(response);
+        });
+      });
+    };
+  }
 
-                        if (typeof (q[key]) === 'object') {
-                            treatFilters(filters, q[key], keyField);
-                        } else {
-                            const filter = {};
+  count(model) {
+    return () => {
+      return new Promise((resolve, reject) => {
+        const filter = {
+          index: model.table,
+          type: "_doc"
+        };
+        this.client.count(filter, (err, response, status) => {
+          if (err) {
+            reject(err);
+          }
 
-                            if (typeof (q[key]) === 'string') {
-                                filter[`${keyField}.keyword`] = q[key];
-                            } else {
-                                filter[keyField] = q[key];
-                            }
+          resolve(response);
+        });
+      });
+    };
+  }
 
-                            filters.push({
-                                term: filter
-                            });
-                        }
-                    }
+  find(model) {
+    return query => {
+      return new Promise((resolve, reject) => {
+        const treatFilters = (filters, q, prefix = "") => {
+          for (let key of Object.keys(q)) {
+            if (prefix) {
+              prefix += ".";
+            }
+            const keyField = `${prefix}${key}`;
 
-                    return filters;
-                }
+            if (q[key] != null && typeof q[key] === "object") {
+              treatFilters(filters, q[key], keyField);
+            } else if (q[key] != null) {
+              const filter = {};
 
-                const filters = treatFilters([], query);
-                const filter = {
-                    index: model.table,
-                    body: {
-                        query: {
-                            bool: {
-                                must: filters
-                            }
-                        }
-                    }
-                };
+              if (typeof q[key] === "string") {
+                filter[`${keyField}.keyword`] = q[key];
+              } else {
+                filter[keyField] = q[key];
+              }
 
-                this.client.search(filter, (err, response, status) => {
-                    if (err) {
-                        reject(err);
-                    }
+              filters.push({
+                term: filter
+              });
+            }
+          }
 
-                    if (response.hits && response.hits.hits) {
-                        resolve(response.hits.hits.map((item) => item._source));
-                    }
+          return filters;
+        };
 
-                    reject(response);
-                });
-            });
-        }
-    }
+        const filters = treatFilters([], query);
+        const filter = {
+          index: model.table,
+          body: {
+            query: {
+              bool: {
+                must: filters
+              }
+            }
+          }
+        };
+
+        this.client.search(filter, (err, response, status) => {
+          if (err) {
+            reject(err);
+          }
+
+          if (response.hits && response.hits.hits) {
+            resolve(response.hits.hits.map(item => item._source));
+          }
+
+          reject(response);
+        });
+      });
+    };
+  }
 }
 
 module.exports = Elasticsearch;
