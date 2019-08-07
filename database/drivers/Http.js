e6c10b43e461b408d4b0b2969da2bdbb8a0ca427
Luccas Almeida <luccas.almeida@vadetaxi.com.br>
2019-08-07 16:12:28 -0300

    fix regex replace, http driver

--- database/drivers/Http.js
+++ database/drivers/Http.js
@@ -1,149 +1,163 @@
-const Driver = require('./Driver');
-const BaseModel = require('../models/BaseModel');
+const Driver = require("./Driver");
+const BaseModel = require("../models/BaseModel");
 
 class Http extends Driver {
-    constructor(config) {
-        super();
+  constructor(config) {
+    super();
 
-        this.host = config.host;
-        this.defaultHeaders = config.headers || {};
-    }
+    this.host = config.host;
+    this.defaultHeaders = config.headers || {};
+  }
 
-    initialize(model) {
-        model.host = this.host;
-        model.defaultHeaders = this.defaultHeaders;
-        model.params = []
+  initialize(model) {
+    model.host = this.host;
+    model.defaultHeaders = this.defaultHeaders;
+    model.params = [];
 
-        model._setMethod = (method) => {
-            model.method = method;
-            return model;
+    model._setMethod = method => {
+      model.method = method;
+      return model;
+    };
+
+    model.get = () => model._setMethod("get");
+    model.post = () => model._setMethod("post");
+    model.put = () => model._setMethod("put");
+    model.delete = () => model._setMethod("delete");
+    model.patch = () => model._setMethod("patch");
+
+    model.setQuery = query => {
+      model.query = query;
+      return model;
+    };
+
+    model.addQuery = query => {
+      model.query = {
+        ...model.query,
+        query
+      };
+      return model;
+    };
+
+    model.setData = data => {
+      model.data = data;
+      return model;
+    };
+
+    model.addData = data => {
+      model.data = {
+        ...model.data,
+        data
+      };
+      return model;
+    };
+
+    model.setHeaders = headers => {
+      model.headers = headers;
+      return model;
+    };
+
+    model.setParam = (name, value) => {
+      model.params = [
+        ...model.params,
+        {
+          name,
+          value
         }
+      ];
 
-        model.get = () => model._setMethod('get');
-        model.post = () => model._setMethod('post');
-        model.put = () => model._setMethod('put');
-        model.delete = () => model._setMethod('delete');
-        model.patch = () => model._setMethod('patch');
+      return model;
+    };
 
-        model.setQuery = (query) => {
-            model.query = query;
-            return model;
-        }
+    model.addHeaders = headers => {
+      model.headers = {
+        ...model.headers,
+        headers
+      };
+      return model;
+    };
 
-        model.addQuery = (query) => {
-            model.query = {
-                ...model.query,
-                query
-            };
-            return model;
-        }
+    model.setSuffix = suffix => {
+      model.suffix = suffix;
+      return model;
+    };
 
-        model.setData = (data) => {
-            model.data = data;
-            return model;
-        }
+    model.addSuffix = suffix => {
+      model.suffix = `${model.suffix ? model.suffix : ""}${suffix}`;
+      return model;
+    };
 
-        model.addData = (data) => {
-            model.data = {
-                ...model.data,
-                data
-            };
-            return model;
-        }
+    model.setId = id => {
+      return this.setSuffix(`/${id}`);
+    };
 
-        model.setHeaders = (headers) => {
-            model.headers = headers;
-            return model;
-        }
+    model.setTimeout = (timeout, timeoutDefault) => {
+      model.timeout = timeout;
+      model.timeoutDefault = timeoutDefault;
 
-        model.setParam = (name, value) => {
-            models.params = [
-                ...models.params,
-                {
-                    name,
-                    value
-                }
+      return model;
+    };
 
-            ]
+    model.call = async () => {
+      let queryParams = "";
+      if (
+        typeof model.query == "object" &&
+        Object.keys(model.query).length > 0
+      ) {
+        queryParams = `?${encodeURIComponent(model.query)}`;
+      }
 
-            return model;
-        }
+      let suffix = "";
+      if (model.suffix != null) {
+        suffix = model.suffix;
+      }
 
-        model.addHeaders = (headers) => {
-            model.headers = {
-                ...model.headers,
-                headers
-            };
-            return model;
-        }
+      let url = model.params.reduce(
+        (prev, curr) =>
+          prev.replace(new RegExp(`{${curr.name}}`, "g"), curr.value),
+        `${model.host}${model.table}${suffix}${queryParams}`
+      );
 
-        model.setSuffix = (suffix) => {
-            model.suffix = suffix;
-            return model;
-        }
+      const callObject = {
+        method: model.method || "get",
+        url
+      };
 
-        model.addSuffix = (suffix) => {
-            model.suffix = `${model.suffix ? model.suffix : ''}${suffix}`;
-            return model;
-        }
+      if (
+        (typeof model.headers == "object" &&
+          Object.keys(model.headers).length > 0) ||
+        (typeof model.defaultHeaders == "object" &&
+          Object.keys(model.defaultHeaders).length > 0)
+      ) {
+        callObject.headers = {
+          ...model.headers,
+          ...model.defaultHeaders
+        };
+      }
 
-        model.setId = (id) => {
-            return this.setSuffix(`/${id}`);
-        }
+      if (typeof model.data == "object" && Object.keys(model.data).length > 0) {
+        callObject.data = model.data;
+      }
 
-        model.setTimeout = (timeout, timeoutDefault) => {
-            model.timeout = timeout;
-            model.timeoutDefault = timeoutDefault;
+      const promises = [];
 
-            return model;
-        }
+      const request = require("axios")(callObject).then(
+        response => response.data
+      );
+      promises.push(request);
 
-        model.call = async () => {
-            let queryParams = '';
-            if (typeof model.query == 'object' && Object.keys(model.query).length > 0) {
-                queryParams = `?${encodeURIComponent(model.query)}`;
-            }
+      if (model.timeout) {
+        promises.push(
+          new Promise(resolve => {
+            setTimeout(() => {
+              resolve(model.timeoutDefault || null);
+            }, model.timeout);
+          })
+        );
+      }
 
-            let suffix = ''
-            if (model.suffix != null) {
-                suffix = model.suffix
-            }
-
-            let url = model.params.reduce((prev, curr) =>
-                prev.replace(new RegExp(curr.name, 'g'), curr.value), `${model.host}${model.table}${suffix}${queryParams}`)
-
-            const callObject = {
-                method: model.method || 'get',
-                url
-            };
-
-            if ((typeof model.headers == 'object' && Object.keys(model.headers).length > 0) || (typeof model.defaultHeaders == 'object' && Object.keys(model.defaultHeaders).length > 0)) {
-                callObject.headers = {
-                    ...model.headers,
-                    ...model.defaultHeaders
-                };
-            }
-
-            if (typeof model.data == 'object' && Object.keys(model.data).length > 0) {
-                callObject.data = model.data;
-            }
-
-            const promises = [];
-
-            const request = require('axios')(callObject).then((response) => response.data);
-            promises.push(request);
-
-            if (model.timeout) {
-                promises.push(new Promise(resolve => {
-                    setTimeout(() => {
-                        resolve(model.timeoutDefault || null);
-                    }, model.timeout);
-                }));
-            }
-
-            return await Promise.race(promises);
-        }
-    }
+      return await Promise.race(promises);
+    };
+  }
 }
 
 module.exports = Http;
